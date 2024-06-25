846551a5d86d4745bbde9f8578922cfcc9ad9ae2
gstecca <giuseppe.stecca@gmail.com>
2024-06-25 19:43:18 +0200

    Functioning realstic instance R2

--- params.py
+++ params.py
@@ -1,10 +1,10 @@
-max_runtime = 60*60*12
+max_runtime = 60*5
 LOAD_EDGES = True # if true load edges. If false compute euclidian distances.
 D2T = 1 # distance to time factor
 D2C = 1 # distance to cost factor
 WRITE_EDGES = False # if true write all edges to excel file
 WRITE_LP = False
-FIX_SOLUTION = False # if we have to read solution from other instance
+FIX_SOLUTION = True # if we have to read solution from other instance
 INIT_SOL = False # if true initialize x 
 LOWER_BOUND = False # if true the lower bound procedure will be activated
 VALID_INEQUALITIES = False

e92ad4c0f3c1453126399485de44f91228a2ebb2
gstecca <giuseppe.stecca@gmail.com>
2024-09-10 17:15:53 +0200

    improved run experiments scipt

--- run_experiments.py
+++ run_experiments.py
@@ -6,7 +6,9 @@ Direc = "instances"
 files = os.listdir(Direc)
 # Filtering only the files.
 names = [f[0:-5] for f in files if os.path.isfile(Direc+'/'+f)]
-#print(*names, sep="\n")
+names = [n for n in names if n[-4:] != "mean"]
 names = sorted(names, reverse=True)
+print(*names, sep="\n")
+#exit()
 for name in names:
     subprocess.run(['python', 'main.py', name])

677ba3301a80268216a2fd470340537861582189
gstecca <giuseppe.stecca@gmail.com>
2024-09-11 19:33:43 +0200

    table results

--- run_experiments.py
+++ run_experiments.py
@@ -1,7 +1,9 @@
 import subprocess
 from os import listdir
+import pandas as pd
 
 import os
+RUN = False
 Direc = "instances"
 files = os.listdir(Direc)
 # Filtering only the files.
@@ -10,5 +12,37 @@ names = [n for n in names if n[-4:] != "mean"]
 names = sorted(names, reverse=True)
 print(*names, sep="\n")
 #exit()
+if RUN:
+    for name in names:
+        subprocess.run(['python', 'main.py', name])
+
+# aggregate
+df = pd.DataFrame(columns=['name', 'objVal', 'VSSobjVal', 'Delta', 'runTime', 'gap', 'Z1', 'Z2', 'Z3', 'Z4', 'Z5', 
+                           'Z1VSS', 'Z2VSS', 'Z3VSS', 'Z4VSS', 'Z5VSS'])
+
 for name in names:
-    subprocess.run(['python', 'main.py', name])
+    nameres = name + "_res.xlsx"
+    nameresfix = name + "_res_fix.xlsx"
+    path ='results/'
+    dfres = pd.read_excel(path + nameres, sheet_name='general', index_col='param')
+    dfresfix = pd.read_excel(path + nameresfix, sheet_name='general', index_col='param')
+
+    nInst = dfres.loc['Inst name']['value']
+    objValue = dfres.loc['objValue']['value']
+    runTime = dfres.loc['runtime']['value']
+    gap = dfres.loc['gap']['value']
+    Z1 = dfres.loc['Z1']['value']
+    Z2 = dfres.loc['Z2']['value']
+    Z3 = dfres.loc['Z3']['value']
+    Z4 = dfres.loc['Z4']['value']
+    Z5 = dfres.loc['Z5']['value']
+    objValueF = dfresfix.loc['objValue']['value']
+    Z1F = dfresfix.loc['Z1']['value']
+    Z2F = dfresfix.loc['Z2']['value']
+    Z3F = dfresfix.loc['Z3']['value']
+    Z4F = dfresfix.loc['Z4']['value']
+    Z5F = dfresfix.loc['Z5']['value']
+    DELTA = (objValueF - objValue)*100/objValue
+    df.loc[len(df)] = [nInst, objValue, objValueF, DELTA, runTime, gap, Z1, Z2, Z3, Z4, Z5, 
+                           Z1F, Z2F, Z3F, Z4F, Z5F]
+df.to_excel('table_results.xlsx')
